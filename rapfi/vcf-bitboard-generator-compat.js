42f909cbf5abb0ceca3a3783b1ada7dcd8f9a676
5871224 <110716199+5871224@users.noreply.github.com>
2026-07-22 15:25:01 +0800

    讓題目產生器使用 C++ 同線雙四相容介面

---
+++ rapfi/vcf-bitboard-generator-compat.js
@@ -0,0 +1,32 @@
+"use strict";
+
+(function installGeneratorCompatibility(global) {
+  const service = global.VCFBitboard;
+  if (!service) return;
+  const originalReady = service.syncReady;
+  const compatReady = originalReady.then(() => {
+    const levelPoint = service.syncModule.cwrap(
+      "vcfBbLegacyGetLevelPointCompat",
+      "number",
+      ["number", "number", "number", "number"],
+    );
+    const lineFour = service.syncModule.cwrap(
+      "vcfBbLegacyTestLineFourCompat",
+      "number",
+      ["number", "number", "number", "number", "number"],
+    );
+    global.getLevelPoint = (idx, color, arr) => {
+      service.writeSyncBoard(arr);
+      return levelPoint(service.syncBoardPtr, idx, color, service.rules);
+    };
+    global.testLineFour = (idx, direction, color, arr) => {
+      service.writeSyncBoard(arr);
+      return lineFour(service.syncBoardPtr, idx, direction, color, service.rules);
+    };
+    global.testLine = global.testLineFour;
+    global.testLineThree = global.testLineFour;
+    return true;
+  });
+  service.syncReady = compatReady;
+  service.compatReady = compatReady;
+})(window);
